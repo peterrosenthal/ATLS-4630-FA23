d5af675ec071cde159c1248047d89c46fddfd235
Peter Rosenthal <peter@rosenth.al>
2023-10-05 16:44:25 -0600

    in-class example from 10-03

---
+++ 10-03/src/index.js
@@ -0,0 +1,5 @@
+import _ from 'lodash';
+
+const myArray = ['a', 'b', 'c', 'd', 'e', 'f'];
+
+console.log(_.chunk(myArray, 4));
---
+++ 10-03/webpack.config.js
@@ -0,0 +1,16 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    })
+  ]
+};
