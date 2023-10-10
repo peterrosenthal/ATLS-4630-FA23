79f2cef427bb01c4cf493d4dd0c52a5e57fc966d
Peter Rosenthal <peter@rosenth.al>
2023-10-10 17:35:31 -0600

    in-class demo from 10-10

---
+++ 10-10/src/index.js
@@ -0,0 +1,7 @@
+import './style.css';
+import squirrel from '../images/squirrel.png';
+import data from './data.csv';
+
+console.log(data);
+
+document.getElementById('squirrel-img').src = squirrel;
